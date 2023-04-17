2d0e85f432a4eb2d9f75503bb8f3f08e77c842fc
DevKimsunho <ksssk96@gmail.com>
2023-04-17 22:37:52 +0900

    23.4.17

--- quantity.py
+++ quantity.py
@@ -2,28 +2,21 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 
-# read excel file
+# Read the data from an Excel file
 df = pd.read_excel('data.xlsx', engine='openpyxl')
 
-# Normalize text in all columns
-df = df.apply(lambda x: x.str.lower().str.strip() if x.dtype == "object" else x)
+# Normalize text in the 'item' column
+df['item'] = df['item'].str.lower().str.strip()
 
-# Write the updated data to a new Excel file
-df.to_excel('output_file.xlsx', index=False)
+# Group by item and sum quantities
+item_counts = df.groupby('item')['quantity'].sum().reset_index()
 
-# group by item and sum quantities
-item_counts = df.groupby('item')['quantity'].sum()
-
-# output the shipment quantity for each item
-for item, count in item_counts.items():
-    print(f'{item}: {count}')
-
-
-# Read the normalized data from an Excel file
-df = pd.read_excel('output_file.xlsx')
+# Output the shipment quantity for each item
+for i, row in item_counts.iterrows():
+    print(f"{row['item']}: {row['quantity']}")
 
 # Extract the item names
-items = df['item'].tolist()
+items = item_counts['item'].tolist()
 
 # Vectorize the item names using the TF-IDF algorithm
 vectorizer = TfidfVectorizer()
@@ -35,7 +28,7 @@ kmeans = KMeans(n_clusters=5, random_state=0).fit(X)
 # Print the cluster labels for each item
 labels = kmeans.labels_
 for i, item in enumerate(items):
-    print(f'{item}: {labels[i]}')    
+    print(f'{item}: {labels[i]}')
 
 
     # 파이썬 실행 파일화
