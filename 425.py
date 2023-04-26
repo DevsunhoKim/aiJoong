e33f5f925f5c5f3ed60212621ffe8c685181397a
DevKimsunho <ksssk96@gmail.com>
2023-04-26 18:02:28 +0900

    23.4.26-2

--- 425.py
+++ 425.py
@@ -1,12 +1,14 @@
 import pandas as pd
+import datetime
 
-# Read the Excel file into a DataFrame
+# Read the Excel file named 'data1.xlsx' into a DataFrame.
 df = pd.read_excel('data1.xlsx')
 
-# Split each item by spaces and count the frequency of complete items and substrings
+# Iterate through each row of the DataFrame, splitting the row into individual items and counting the frequency of each item.
 freq = {}
 for item in df.iloc[:,0]:
-    # Check for the existence of multi-word items before splitting by spaces
+    # Convert the dict that stores item frequencies to a DataFrame and sort by frequency in descending order.
+    # Exceptions are made for items that contain spaces.
     if "눈부시게 아름다운 사진들" in item:
         substring = "눈부시게 아름다운 사진들"
         if substring not in freq:
@@ -21,20 +23,23 @@ for item in df.iloc[:,0]:
                 freq[substring] = 1
             else:
                 freq[substring] += 1
-        # Check for the existence of multi-word items after splitting by spaces
+    
         if "보기드문 사진들" in item:
             substring = "보기드문 사진들"
             if substring not in freq:
                 freq[substring] = 1
             else:
                 freq[substring] += 1
-        elif "rare photos" in item:
-            substring = "rare photos"
-            if substring not in freq:
-                freq[substring] = 1
-            else:
-                freq[substring] += 1
 
-# Print the frequency for each complete item and substring
+# Create a DataFrame from the frequency dictionary and sort by frequency in descending order.
+df_freq = pd.DataFrame(freq.items(), columns=['substring', 'count']).sort_values(by=['count'], ascending=False)
+
+# Generate file name with current date.
+file_name = datetime.datetime.now().strftime("%Y-%m-%d") + '.csv'
+
+# Write the DataFrame to a CSV file with the current date as the file name, specifying encoding and delimiter.
+df_freq.to_csv(file_name, index=False, encoding='utf-8', sep=',')
+
+# Print the frequency count for each item.
 for substring, count in freq.items():
     print(substring, count)
