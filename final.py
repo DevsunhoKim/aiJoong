f7274f18fe936b04e3d42e53d81c5786742bdf4c
DevKimsunho <ksssk96@gmail.com>
2023-06-08 18:53:54 +0900

    23.6.8

--- final.py
+++ final.py
@@ -1,13 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from datetime import date
+import matplotlib.font_manager as fm
 
-# Read the Excel file
+
 excel_file = pd.read_excel('data1.xlsx', usecols='A')
 
-# Get the values from the desired column
+
 file_names = excel_file.iloc[:, 0].tolist()
 
-# Calculate the quantity of each item
+
 tally = {}
 for name in file_names:
     if name in tally:
@@ -15,19 +17,42 @@ for name in file_names:
     else:
         tally[name] = 1
 
-# Create a DataFrame with the tally results
+
 output_df = pd.DataFrame({'Item': list(tally.keys()), 'Quantity': list(tally.values())})
 
-# Sort the DataFrame by quantity in descending order
+
 output_df = output_df.sort_values(by='Quantity', ascending=False)
 
-# Get the current date
+
 today = date.today().strftime("%Y%m%d")
 
-# Set the output file name using the current date
+
 output_file = f"tally_results_{today}.xlsx"
 
-# Write the results to the output file
+
 output_df.to_excel(output_file, index=False)
 
+
+plt.bar(output_df['Item'], output_df['Quantity'])
+plt.xlabel('Item')
+plt.ylabel('Quantity')
+plt.title('Tally Results')
+plt.xticks(rotation=90)
+plt.show()
+
 print(f"Tally results have been written to {output_file}.")
+
+
+font_path = '/usr/share/fonts/truetype/nanum/NanumBarunGothic.ttf'
+fontprop = fm.FontProperties(fname=font_path, size=12)
+
+
+plt.bar(output_df['Item'], output_df['Quantity'])
+plt.xlabel('품목', fontproperties=fontprop)  
+plt.ylabel('수량', fontproperties=fontprop)  
+plt.title('품목별 수량 결과', fontproperties=fontprop)  
+plt.xticks(rotation=90, fontproperties=fontprop)  
+plt.show()
+
+
+
